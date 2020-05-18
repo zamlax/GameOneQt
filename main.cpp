0e9d9c8db5603cc6a6d6fd296dec30d38467f200
zamlax <65530583+zamlax@users.noreply.github.com>
2020-05-18 12:19:09 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include "Game.h"
 
-int main(int argc, char *argv[])
-{
+/*
+Tutorial Topics:
+-QGraphicsPixmapItem, QPixmap, QImage
+*/
+
+Game * game;
+
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    game = new Game();
+    game->show();
+
     return a.exec();
 }
